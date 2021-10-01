2e24fab0aee9293445a1193e8bc4142953617de7
JoseMarshall <hebojosemar@gmail.com>
2021-10-01 12:36:30 +0100

    feat: FizzBuzz class

---
+++ src/main/java/com/mycompany/objective/FizzBuzz.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.objective;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+/**
+ *
+ * @author User
+ */
+public class FizzBuzz {
+
+    private Map<Integer, String> rules;
+
+    public FizzBuzz() {
+        this.rules = new HashMap();
+    }
+
+    public FizzBuzz(HashMap<Integer, String> rules) {
+        this.rules = rules;
+    }
+
+    public void addRule(int key, String str) {
+        rules.put(key, str);
+    }
+
+    public void removeRule(int key) {
+        rules.remove(key);
+    }
+
+    public Map<Integer, String> getRules() {
+        return rules;
+    }
+
+    public void setRules(Map<Integer, String> rules) {
+        this.rules = rules;
+    }
+
+    public List<String> generate(int start, int end) {
+        String str;
+        var result = new ArrayList();
+        for (Integer i = start; i <= end; i++) {
+            str = "";
+            for (Entry<Integer, String> entry : rules.entrySet()) {
+                str += concatStr(i, entry.getKey(), entry.getValue());
+            }
+            result.add(str.equals("") ? i.toString() : str);
+        }
+
+        return result;
+    }
+
+    private static String concatStr(int i, int n, String str) {
+        return (i % n == 0) ? str : "";
+    }
+
+}
